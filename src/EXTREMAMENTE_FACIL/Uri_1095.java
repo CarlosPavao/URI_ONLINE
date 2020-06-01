4cc42d7b7b28c3d1b585bc8409b38c335dcc1c70
Carlos Pavão <carlos.henrique93@msn.com>
2020-06-01 13:01:09 -0300

    novas Resoluções

---
+++ src/EXTREMAMENTE_FACIL/Uri_1095.java
@@ -0,0 +1,11 @@
+package EXTREMAMENTE_FACIL;
+
+public class Uri_1095 {
+    
+    public static void main(String[] args) {
+        for(int I = 1,J = 60;J >= 0; I+=3,J-=5){
+			System.out.print("I="+I+" J="+J+"\n");
+		}
+
+    }
+}
