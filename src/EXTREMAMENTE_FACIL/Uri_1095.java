f3292feb9c67c867fa5565ad53b2d08a0a99052d
Carlos Pavão <carlos.henrique93@msn.com>
2020-07-24 11:59:59 -0300

    Inicio Orientação a objeto

--- src/EXTREMAMENTE_FACIL/Uri_1095.java
+++ src/EXTREMAMENTE_FACIL/Uri_1095.java
@@ -1,9 +1,5 @@
 package EXTREMAMENTE_FACIL;
-<<<<<<< HEAD
 // @author Carlos Pavão <carlos.henrique93@msn.com>
-=======
-
->>>>>>> 4cc42d7b7b28c3d1b585bc8409b38c335dcc1c70
 public class Uri_1095 {
     
     public static void main(String[] args) {
---
+++ src/EXTREMAMENTE_FACIL/Uri_1113.java
@@ -0,0 +1,31 @@
+package EXTREMAMENTE_FACIL;
+/* 
+Leia uma quantidade indeterminada de duplas de valores inteiros X e Y. 
+Escreva para cada X e Y uma mensagem que indique se estes valores foram 
+digitados em ordem crescente ou decrescente.
+Entrada:
+A entrada contém vários casos de teste. Cada caso contém dois valores 
+inteiros X e Y. A leitura deve ser encerrada ao ser fornecido valores 
+iguais para X e Y.
+Saída:
+Para cada caso de teste imprima “Crescente”, caso os valores tenham sido 
+digitados na ordem crescente, caso contrário imprima a mensagem “Decrescente”.
+ */
+import java.util.Scanner;
+
+public class Uri_1113 {
+// @author Carlos Pavão <carlos.henrique93@msn.com>
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        int a, b;
+        
+        while(!((a=entrada.nextInt()) == (b=entrada.nextInt()))){
+            if(a>b){
+                System.out.println("Decrescente");
+            }else{
+                System.out.println("Crescente");            
+            }
+        }
+    entrada.close();
+    }
+}
